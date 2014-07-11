bd26b41e69f59ecdc30e2cced79f23eaea30cf2c
Shawn <hanlong522@gmail.com>
2014-07-11 16:19:43 +0800

    first commit

--- cameraDemo/AppDelegate.m
+++ cameraDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CameraViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    CameraViewController *cameraView = [[CameraViewController alloc] init];
+    self.window.rootViewController = cameraView;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ cameraDemo/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  cameraDemo
+//
+//  Created by Shawn on 14-7-11.
+//  Copyright (c) 2014年 hanlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
