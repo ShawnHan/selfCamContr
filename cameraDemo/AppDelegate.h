d1945e7b29108f3b27590d4830c046faab970d37
Shawn <hanlong522@gmail.com>
2014-07-11 16:14:20 +0800

    Initial Commit

---
+++ cameraDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cameraDemo
+//
+//  Created by Shawn on 14-7-11.
+//  Copyright (c) 2014年 hanlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cameraDemoTests/cameraDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  cameraDemoTests.m
+//  cameraDemoTests
+//
+//  Created by Shawn on 14-7-11.
+//  Copyright (c) 2014年 hanlong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cameraDemoTests : XCTestCase
+
+@end
+
+@implementation cameraDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
